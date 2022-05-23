0f19140fb341c7c8ec79e5fddf0ef93e7890b49d
tuguzT <timurka.tugushev@gmail.com>
2022-05-23 11:25:41 +0300

    Provide custom top app bar to its own composable, fix top search bar colors

--- src/main/kotlin/io/github/tuguzt/ddbms/practice8/view/MainScreen.kt
+++ src/main/kotlin/io/github/tuguzt/ddbms/practice8/view/MainScreen.kt
@@ -12,7 +12,10 @@ import androidx.compose.ui.draw.clip
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.unit.dp
 import io.github.tuguzt.ddbms.practice8.model.Mock
-import io.github.tuguzt.ddbms.practice8.view.utils.*
+import io.github.tuguzt.ddbms.practice8.view.utils.CollectionTablePreview
+import io.github.tuguzt.ddbms.practice8.view.utils.ExposedDropdownMenu
+import io.github.tuguzt.ddbms.practice8.view.utils.SearchBar
+import io.github.tuguzt.ddbms.practice8.view.utils.Tooltip
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
 import org.litote.kmongo.coroutine.CoroutineDatabase
@@ -23,101 +26,121 @@ fun MainScreen(
     coroutineScope: CoroutineScope = rememberCoroutineScope(),
 ) {
     val collection = remember { database.getCollection<Mock>("mock") }
-    var searchQuery by remember { mutableStateOf("") }
 
     val snackbarHostState = remember { SnackbarHostState() }
     val focusManager = LocalFocusManager.current
 
     Scaffold(
-        modifier = Modifier
-            .clickable(
-                interactionSource = remember { MutableInteractionSource() },
-                indication = null,
-                onClick = focusManager::clearFocus,
-            ),
+        modifier = Modifier.clickable(
+            interactionSource = remember { MutableInteractionSource() },
+            indication = null,
+            onClick = focusManager::clearFocus,
+        ),
         scaffoldState = rememberScaffoldState(snackbarHostState = snackbarHostState),
         topBar = {
-            val color = MaterialTheme.colors.primarySurface
-            val contentColor = contentColorFor(color)
-            Surface(
-                color = color,
-                contentColor = contentColor,
-                elevation = AppBarDefaults.TopAppBarElevation,
-            ) {
-                CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
-                    var expanded by remember { mutableStateOf(false) }
-                    val suggestions = listOf("Item1", "Item2", "Item3")
-
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .padding(16.dp),
-                        verticalAlignment = Alignment.CenterVertically,
-                    ) {
-                        Tooltip(
-                            text = "Collection name",
-                            modifier = Modifier
-                                .width(128.dp)
-                                .heightIn(min = 56.dp),
-                        ) {
-                            ExposedDropdownMenu(
-                                items = suggestions,
-                                dropdownType = "collection",
-                                expanded = expanded,
-                                onExpandedChange = { expanded = it },
-                                onItemSelected = { item ->
-                                    coroutineScope.launch {
-                                        snackbarHostState.showSnackbar(
-                                            message = "Item \"${item.orEmpty()}\" was chosen",
-                                            actionLabel = "Dismiss",
-                                        )
-                                    }
-                                },
-                                modifier = Modifier
-                                    .width(128.dp)
-                                    .heightIn(min = 56.dp),
-                                color = MaterialTheme.colors.onSurface
-                                    .copy(alpha = TextFieldDefaults.BackgroundOpacity),
-                                shape = RoundedCornerShape(4.dp),
-                            )
-                        }
-                        Spacer(modifier = Modifier.width(16.dp))
-
-                        Tooltip(text = "Search bar", modifier = Modifier.fillMaxWidth()) {
-                            SearchBar(
-                                value = searchQuery,
-                                onValueChange = { searchQuery = it },
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .clip(RoundedCornerShape(4.dp)),
-                                onSubmit = {
-                                    focusManager.clearFocus()
-                                    coroutineScope.launch {
-                                        snackbarHostState.showSnackbar(
-                                            message = "Searching by query \"$searchQuery\"",
-                                            actionLabel = "Dismiss",
-                                        )
-                                    }
-                                },
-                                singleLine = true,
-                                colors = TextFieldDefaults.textFieldColors(
-                                    placeholderColor = contentColor.copy(alpha = ContentAlpha.medium),
-                                    focusedIndicatorColor = contentColor.copy(alpha = ContentAlpha.medium),
-                                    unfocusedIndicatorColor = contentColor.copy(alpha = ContentAlpha.disabled),
-                                    leadingIconColor = contentColor.copy(alpha = TextFieldDefaults.IconOpacity),
-                                    trailingIconColor = contentColor.copy(alpha = TextFieldDefaults.IconOpacity),
-                                    focusedLabelColor = contentColor.copy(alpha = ContentAlpha.medium),
-                                    unfocusedLabelColor = contentColor.copy(alpha = ContentAlpha.high),
-                                ),
-                            )
-                        }
+            TopBar(
+                onSubmit = { searchText ->
+                    focusManager.clearFocus()
+                    coroutineScope.launch {
+                        snackbarHostState.showSnackbar(
+                            message = "Searching by \"$searchText\"",
+                            actionLabel = "Dismiss",
+                        )
                     }
-                }
-            }
+                },
+                dropdownItems = List(5) { "Item ${it + 1}" },
+                onDropdownItemSelected = { item ->
+                    coroutineScope.launch {
+                        snackbarHostState.showSnackbar(
+                            message = "Item \"${item.orEmpty()}\" was chosen",
+                            actionLabel = "Dismiss",
+                        )
+                    }
+                },
+            )
         },
     ) { padding ->
         Column(modifier = Modifier.padding(padding)) {
             CollectionTablePreview()
         }
     }
+}
+
+@Composable
+private fun TopBar(
+    onSubmit: (String) -> Unit,
+    dropdownItems: List<String>,
+    onDropdownItemSelected: (String?) -> Unit,
+) {
+    Surface(
+        color = MaterialTheme.colors.primarySurface,
+        elevation = AppBarDefaults.TopAppBarElevation,
+    ) {
+        CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+            var searchText by remember { mutableStateOf("") }
+            var dropdownExpanded by remember { mutableStateOf(false) }
+
+            Row(
+                modifier = Modifier.fillMaxWidth().padding(16.dp),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Tooltip(text = "Collection name", modifier = Modifier.width(128.dp).heightIn(min = 56.dp)) {
+                    ExposedDropdownMenu(
+                        items = dropdownItems,
+                        dropdownType = "collection",
+                        expanded = dropdownExpanded,
+                        onExpandedChange = { dropdownExpanded = it },
+                        onItemSelected = onDropdownItemSelected,
+                        modifier = Modifier.width(128.dp).heightIn(min = 56.dp),
+                        color = MaterialTheme.colors.onSurface.copy(alpha = TextFieldDefaults.BackgroundOpacity),
+                        shape = RoundedCornerShape(4.dp),
+                    )
+                }
+                Spacer(modifier = Modifier.width(16.dp))
+                Tooltip(text = "Search bar", modifier = Modifier.fillMaxWidth()) {
+                    TopSearchBar(
+                        value = searchText,
+                        onValueChange = { searchText = it },
+                        modifier = Modifier.fillMaxWidth().clip(RoundedCornerShape(4.dp)),
+                        onSubmit = { onSubmit(searchText) },
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun TopSearchBar(
+    value: String,
+    onValueChange: (String) -> Unit,
+    onSubmit: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    SearchBar(
+        value = value,
+        onValueChange = onValueChange,
+        modifier = modifier,
+        onSubmit = onSubmit,
+        singleLine = true,
+        colors = run {
+            val color = MaterialTheme.colors.primarySurface
+            val contentColor = contentColorFor(color)
+            val focusColor = when {
+                MaterialTheme.colors.isLight -> contentColor
+                else -> MaterialTheme.colors.primary
+            }
+
+            TextFieldDefaults.textFieldColors(
+                placeholderColor = contentColor.copy(alpha = ContentAlpha.medium),
+                cursorColor = focusColor,
+                focusedIndicatorColor = focusColor.copy(alpha = ContentAlpha.medium),
+                unfocusedIndicatorColor = focusColor.copy(alpha = ContentAlpha.disabled),
+                leadingIconColor = contentColor.copy(alpha = TextFieldDefaults.IconOpacity),
+                trailingIconColor = contentColor.copy(alpha = TextFieldDefaults.IconOpacity),
+                focusedLabelColor = focusColor.copy(alpha = ContentAlpha.medium),
+                unfocusedLabelColor = focusColor.copy(alpha = ContentAlpha.high),
+            )
+        },
+    )
 }
